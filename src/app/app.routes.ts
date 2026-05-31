9f42fc47202ff301348852158d1bd54611028667
Justin <khanjal@gmail.com>
2026-05-31 14:21:31 -0400

    Simplify projects to single generic page; remove per-project detail route/component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ProjectsListComponent } from './projects/projects-list.component';
-import { ProjectDetailComponent } from './projects/project-detail.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'projects', component: ProjectsListComponent },
-  { path: 'projects/:id', component: ProjectDetailComponent },
   { path: '**', redirectTo: '' },
 ];
--- src/app/projects/projects-list.component.ts
+++ src/app/projects/projects-list.component.ts
@@ -1,6 +1,5 @@
 import { Component, inject } from '@angular/core';
 import { SITE_CONFIG } from '../site.config';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-projects-list',
@@ -11,10 +10,4 @@ import { Router } from '@angular/router';
 })
 export class ProjectsListComponent {
   config = SITE_CONFIG;
-  private router = inject(Router);
-
-  open(title: string) {
-    const id = encodeURIComponent(title.toLowerCase().replace(/\s+/g, '-'));
-    this.router.navigate(['/projects', id]);
-  }
 }
